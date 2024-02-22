1dc785b95ac6d367437658d6ce568245d3e0c4dd
andrew <113289014+aagb1884@users.noreply.github.com>
2024-02-22 15:29:56 +0000

    Update Footer.jsx

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -21,14 +21,7 @@ function Footer() {
         Made by Andrew Blair
       </a>
       <br />
-      <a
-        className="aboutScreen-personalLink"
-        href="https://ko-fi.com/freelance_liar"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-      <img height='36' style='border:0px;height:36px;' src='https://storage.ko-fi.com/cdn/kofi2.png?v=3' border='0' alt='Support My Work at ko-fi.com' />
-      </a>
+      <a href='https://ko-fi.com/W7W1URM7B' target='_blank'><img height='36' style='border:0px;height:36px;' src='https://storage.ko-fi.com/cdn/kofi2.png?v=3' border='0' alt='Buy Me a Coffee at ko-fi.com' /></a>
     </div>
   );
 }
